ddaca3e79ded55e0598ea0643d7f76b6620cb53d
aeroblade <aeroblade525@gmail.com>
2026-01-03 14:32:26 -0500

    More api tests

--- algorithms/PythonMain.py
+++ algorithms/PythonMain.py
@@ -1,15 +1,15 @@
-from Path_Find_Algorithm.Path_Find_Main import solve_algorithm, solve_cube_internal
-from PythonCubeArray import cube_array_python
+# from Path_Find_Algorithm.Path_Find_Main import solve_algorithm, solve_cube_internal
+# from algorithms.PythonCubeArray import cube_array_python
 
-def main():
-    # print(edge_solver(cube_array_python, (0, 1, 2)))
+# def main():
+#     # print(edge_solver(cube_array_python, (0, 1, 2)))
 
-    print("this is the start", cube_array_python)
-    print("this is the change", solve_algorithm(cube_array_python, solve_cube_internal(cube_array_python)))
-    # print(home_position_corner((0, 0, 0), cube_array_python))
+#     print("this is the start", cube_array_python)
+#     print("this is the change", solve_algorithm(cube_array_python, solve_cube_internal(cube_array_python)))
+#     # print(home_position_corner((0, 0, 0), cube_array_python))
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
-# Overall Scamble partern F2 R' L2 D' B' R' F' B U' F (red front, yellow top)
+# # Overall Scamble partern F2 R' L2 D' B' R' F' B U' F (red front, yellow top)
